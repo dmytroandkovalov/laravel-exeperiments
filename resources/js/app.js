be0a9afba4c8f40c7aa3ccd71c6e8d2470d7b8dc
dmytro kovalov <d.kovalov@ukr.net>
2020-03-01 12:34:45 +0200

    Made demo vue-laravel spa app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
+
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+Vue.config.devtools = true;
+Vue.config.performance = true;
+
+import App from './components/App';
+
+const app = new Vue({
+    el:'#app',
+    components: {
+        App
+    },
+    render: h => h(App)
+});
---
+++ resources/js/components/App.vue
@@ -0,0 +1,72 @@
+<template>
+    <div id="app">
+        <div class="heading">
+            <h1>
+                Cruds
+            </h1>
+        </div>
+        <crud-component
+            v-for="crud in cruds"
+            v-bind="crud"
+            :key="crud.id"
+            @update="update"
+            @delete="del"
+        ></crud-component>
+        <div>
+            <button @click="create">Add</button>
+        </div>
+    </div>
+</template>
+<script>
+    function Crud({id, color, name}) {
+        this.id = id;
+        this.color = color;
+        this.name = name;
+    }
+
+    import CrudComponent from "./CrudComponent";
+
+    export default {
+        data() {
+            return {
+                cruds: [],
+                mute:false
+            }
+        },
+        methods: {
+            async create() {
+                this.mute = true;
+                const {data} = await window.axios.get('/api/cruds/create');
+                this.cruds.push(new Crud(data));
+                this.mute = false;
+
+            },
+            async read() {
+                this.mute = true;
+                const {data} = await window.axios.get('/api/cruds');
+                data.forEach(crud => this.cruds.push(new Crud(crud)));
+                this.mute = false;
+            },
+            async update(id, color) {
+                this.mute = true;
+                await window.axios.put(`/api/cruds/${id}`, {color});
+                this.mute = false;
+
+                this.cruds.find(crud => crud.id === id).color = color;
+            },
+            async del(id) {
+                this.mute = true;
+                await window.axios.delete(`/api/cruds/${id}`);
+                let index = this.cruds.findIndex(crud => crud.id === id);
+                this.cruds.splice(index, 1);
+                this.mute = false;
+            }
+        },
+        components: {
+            CrudComponent
+        },
+        created() {
+            this.read();
+        }
+    }
+</script>
